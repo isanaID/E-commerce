b9a069ca7512a00725dcad5bbce8b53254a2bdc6
isanaid <eizisana@gmail.com>
2022-04-07 23:11:36 +0700

    add auth login and logout to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './app/store';
 import Home from './pages/Home';
 
+import { listen } from './app/listener';
+
 
 function App() {
+
+  React.useEffect(() => {
+    listen();
+  }, []);
+
   return (
     <div className="App">
       <Provider store={store}>
