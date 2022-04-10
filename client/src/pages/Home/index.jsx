d1194f4d6b84bc61e84d20c80716760385dc70e8
isanaid <eizisana@gmail.com>
2022-04-10 21:31:44 +0700

    fix redux state product

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import TopBar from '../../components/TopBar';
 
 export default function Home() {
