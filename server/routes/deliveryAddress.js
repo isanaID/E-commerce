895f2f70feb62d31ec31865160e5501c4ce59a37
isanaid <eizisana@gmail.com>
2022-04-17 16:59:38 +0700

    fix minor error

--- server/routes/deliveryAddress.js
+++ server/routes/deliveryAddress.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const multer = require('multer');
 const upload = multer();
 
-router.get('/deliveryAddress', policies_check('read', 'DeliveryAddress'), deliveryAddressController.index);
+router.get('/delivery-addresses', policies_check('read', 'DeliveryAddress'), deliveryAddressController.index);
 router.post('/delivery-addresses', upload.none(), policies_check('create', 'DeliveryAddress'), deliveryAddressController.store);
 router.put('/delivery-addresses/:id', upload.none(), policies_check('update', 'DeliveryAddress'), deliveryAddressController.update);
 router.delete('/delivery-addresses/:id', policies_check('delete', 'DeliveryAddress'), deliveryAddressController.destroy);
