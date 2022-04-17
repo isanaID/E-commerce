d640088f359e52ca6d2a68e05f01ffb414ef8bf6
isanaid <eizisana@gmail.com>
2022-04-17 14:54:34 +0700

    minor change

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -18,7 +18,6 @@ function decodeToken() {
         } catch (err) {
             if(err && err.name === 'JsonWebTokenError') {
                  return res.json({error: 1, message: err.message});
-                 console.log(err.message);
             }
             next(err);
         }
