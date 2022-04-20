7f3f898d8b3eb256c645166fd26cdc738c23ad6c
isanaid <eizisana@gmail.com>
2022-04-20 14:12:00 +0700

    fix user order

--- client/src/pages/UserOrders/index.jsx
+++ client/src/pages/UserOrders/index.jsx
@@ -1,26 +1,67 @@
 import * as React from 'react'; 
-import {LayoutOne, Table, Button, Text} from 'upkit';
+import {LayoutOne, Button, Text, Table} from 'upkit';
 import { Link } from 'react-router-dom';
-
+import BarLoader from 'react-spinners/BarLoader';
 import TopBar from '../../components/TopBar';
 import StatusLabel from '../../components/StatusLabel';
-// import { formatRupiah } from '../../utils/format-rupiah';
-// import { sumPrice } from '../../utils/sum-price';
+import { formatRupiah } from '../../utils/format-rupiah';
+import { sumPrice } from '../../utils/sum-price';
 import { FaFileInvoiceDollar } from '@meronex/icons/fa/FaFileInvoiceDollar';
 import { getOrders } from '../../api/order';
 
+const columns = [
+  { 
+    Header: 'Order', 
+    id: 'Status',
+    accessor: order => {
+      return <div>
+        #{order.order_number} <br/>
+        <StatusLabel status={order.status}/>
+      </div>
+    }
+  },
+  {
+    Header: 'Items', 
+    accessor: order => {
+      return <div>
+        {order.order_items.map(item => {
+          return <div key={item._id}>
+            {item.name} {item.qty}
+          </div>
+        })}
+      </div>
+    }
+  }, 
+  {
+    Header: 'Total',
+    accessor: order => {
+      return <div>
+        {formatRupiah(sumPrice(order.order_items) + order.delivery_fee)}
+      </div>
+    }
+  },
+  {
+    Header: 'Invoice',
+    accessor: order => {
+      return <div>
+        <a href={`/invoice/${order._id}`}> Lihat Invoice </a>
+      </div>
+    }
+  }
+];
 
 export default function UserOrders(){
-   let [pesanan, setPesanan] = React.useState([]);
-   let [count, setCount] = React.useState(0);
-   let [status, setStatus] = React.useState('idle');
-   let [page, setPage] = React.useState(1);
-   let [limit, ] = React.useState(10);
 
-  let fetchPesanan = React.useCallback( async () => {
+  let [pesanan, setPesanan] = React.useState([]);
+  let [count, setCount] = React.useState(0);
+  let [status, setStatus] = React.useState('idle');
+  let [page, setPage] = React.useState(1);
+  let [limit, ] = React.useState(10);
+
+  const fetchPesanan = React.useCallback( async () => {
     setStatus('process');
 
-    let { data } = await getOrders(page, limit);
+    let { data } = await getOrders({limit, page});
 
     if(data.error){
       setStatus('error');
@@ -37,43 +78,91 @@ export default function UserOrders(){
     fetchPesanan();
   }, [fetchPesanan]);
 
-  console.log(pesanan);
-  
+    return (
+      <div>
+        <TopBar/>
+      <LayoutOne>
+        
+        <Text as="h3"> Pesanan Anda </Text>
+        <br />
 
-   return (
-     <LayoutOne>
-       <TopBar/>
-       <Text as="h3"> Pesanan Anda </Text>
-       <br />
-       <div>
-       <table className="table">
-       <thead>
-          <tr>
-            <th>Order Number</th>
-            <th>Status</th>
-            <th>invoice</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {pesanan.map(order => {
-            return <tr key={order._id}>
-              <td>{order.order_number}</td>
-              <td>
-                <StatusLabel status={order.status}/>
-              </td>
-              <td>
-                <Link to={`/invoice/${order._id}`}>
-                  <Button color="gray" iconBefore={<FaFileInvoiceDollar/>}>
-                    Invoice
-                  </Button>
-                </Link>
-              </td>
-            </tr>
-          })} */}
-        </tbody>
-      </table>
+        <Table
+            color='blue'
+          items={pesanan}
+          totalItems={count}
+          columns={columns}
+          onPageChange={ page => setPage(page)}
+          page={page}
+          isLoading={status === 'process'}
+        />
+
+      </LayoutOne>
+      
       </div>
-
-     </LayoutOne>
-   )
+    )
 }
+
+// function UserOrders() {
+//    let [pesanan, setPesanan] = React.useState([]);
+//    let [count, setCount] = React.useState(0);
+//    let [status, setStatus] = React.useState('idle');
+//    let [page, setPage] = React.useState(1);
+//    let [limit, ] = React.useState(10);
+
+//   let fetchPesanan = React.useCallback( async () => {
+//     setStatus('process');
+
+//     let { data } = await getOrders({limit, page});
+
+//     if(data.error){
+//       setStatus('error');
+//       return;
+//     }
+
+//     setStatus('success');
+//     let dataPesanan = Object.values(data.data);
+//     setPesanan(dataPesanan);
+//     setCount(data.count);
+
+//   }, [page, limit]);
+
+//   React.useEffect(() => {
+//     fetchPesanan();
+//   }, [fetchPesanan]);
+
+//   console.log(pesanan);
+  
+//    return (
+//      <LayoutOne>
+//        <TopBar/>
+//        <Text as="h3"> Pesanan Anda </Text>
+//        <br />
+//         <div>
+//        <table>
+//           <thead>
+//             <tr>
+//             <th>Order Number</th>
+//             <th>Status</th>
+//             <th>Total</th>
+//             <th>Invoice</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             { pesanan.map( (pesanan, index) => (
+//               <tr key={index}>
+//                 <td>{pesanan.order_number}</td>
+//                 <td> <StatusLabel status={pesanan.status} /> </td>
+//                 <td>{formatRupiah(sumPrice(pesanan.order_items) + pesanan.delivery_fee)}</td>
+//                 <td> inv </td>
+//               </tr>
+//             ))}
+//           </tbody>
+                  
+//        </table>
+//        </div>
+
+//      </LayoutOne>
+//    )
+// }
+
+// export default UserOrders;
